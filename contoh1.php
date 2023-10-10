d07e4929fccb0b895ae5aef5bd822563d0f6d63d
napriliaas <145760194+napriliaas@users.noreply.github.com>
2023-10-10 11:04:23 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends   CI_Controller
+{
+    Public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Nia Aprilia
+            Saya tinggal di daerah Tebet olahraga yang saya sukai adalah Renang";
+    }
+}
